336480a61656361bff7d6e2bf2abcdb8ae20905d
viniciusgdr <79378773+viniciusgdr@users.noreply.github.com>
2021-02-26 15:54:01 -0300

    gd

---
+++ garena/src/help.js
@@ -0,0 +1,95 @@
+const help = (prefix) => {
+	return `
+<══════════════════════>
+      *ViniciusBOT - WA DEV BOT*
+ <══════════════════════>
+
+┌─────────────────
+
+🧐 *informações*
+  🐊 Prefix: 「  ${prefix}  」
+  🐊 Criador : Vinicius (base causs)
+  Contato 81983064666
+ 
+🧐 *Helps Secundarios*
+  ├─  ${prefix}ping
+  ├─  ${prefix}nsfwmenu (+18)
+  ├─  ${prefix}bc
+  ├─  ${prefix}bcgc
+  ├─  ${prefix}block
+  ├─  ${prefix}clearall
+  ├─ 🐊 ${prefix}bloquear
+  ̑ͮͫͭͥͦͣ̔͒͑͑̅͊́͋͊͗͑̓̍̽ͬ̋̎̓ͧ͂̊̉ͣ̉ͩ̀̈̏̍ͭ̒͒͂̍̇̐̿ͨͦ̑͑̃̎͒̋͌̌̈́ͩ̔ͩ̉̿̐ͭ̂͛͊ͩ̔͋ͭͩ͑̆̇͊ͫ̃͋̋ͪͦ͐̈́͗ͭ̒̐̿̃͂́̅̉͊ͧͫ̔̄̿̓́̀̄̾ͨ̍ͨͩͩͮ̑ͭͧͦͣ̽̃͛̔̇ͣ̂͗ͪ͛̈́̆ͮͧ̈̔́͛́͊̇͒ͭ̓ͬ̅̑̊ͩ͆̌ͩ͑̒͊ͭ̀̃̏̅̏ͦͣ̏̿ͬ͂̿ͣ́̓͌ͥ̍ͦ̊̄̆̿̿̈́̅̄̐̎͌͋ͦ͋͂ͨ̂̈͂̉̈́̀̒̃̈́͂̃ͤͪ̎͋̆ͤ̽̇ͩ̑ͨ̽͊ͦͩ̓̎̑̎̆̉ͮ̄͑̆͌ͨ̓̀̋ͤͥͯ͋͑ͧ̆̋ͨͯ̔̑̽͗ͬ͒̀̓͐ͯ̏͆̏ͣ̌͌ͩ̏́̈͂̃͋͛̔ͣ̓̓̇̉ͪͩͯ͛̍͑ͯͨͫͯ̔ͯ̾̓͊͐ͯ̑̚̚̚̚̚�̑ͮͫͭͥͦͣ̔͒͑͑̅͊́͋͊͗͑̓̍̽ͬ̋̎̓ͧ͂̊̉ͣ̉ͩ̀̈̏̍ͭ̒͒͂̍̇̐̿ͨͦ̑͑̃̎͒̋͌̌̈́ͩ̔ͩ̉̿̐ͭ̂͛͊ͩ̔͋ͭͩ͑̆̇͊ͫ̃͋̋ͪͦ͐̈́͗ͭ̒̐̿̃͂́̅̉͊ͧͫ̔̄̿̓́̀̄̾ͨ̍ͨͩͩͮ̑ͭͧͦͣ̽̃͛̔̇ͣ̂͗ͪ͛̈́̆ͮͧ̈̔́͛́͊̇͒ͭ̓ͬ̅̑̊ͩ͆̌ͩ͑̒͊ͭ̀̃̏̅̏ͦͣ̏̿ͬ͂̿ͣ́̓͌ͥ̍ͦ̊̄̆̿̿̈́̅̄̐̎͌͋ͦ͋͂ͨ̂̈͂̉̈́̀̒̃̈́͂̃ͤͪ̎͋̆ͤ̽̇ͩ̑ͨ̽͊ͦͩ̓̎̑̎̆̉ͮ̄͑̆͌ͨ̓̀̋ͤͥͯ͋͑ͧ̆̋ͨͯ̔̑̽͗ͬ͒̀̓͐ͯ̏͆̏ͣ̌͌ͩ̏́̈͂̃͋͛̔ͣ̓̓̇̉ͪͩͯ͛̍͑ͯͨͫͯ̔ͯ̾̓͊͐ͯ̑̚̚̚̚̚
+  |───*Cadastramento*
+  │
+  ├─ 🐊 ${prefix}cadastrar + seu nome (cadastre-se no bot)
+  ├>>>Necessário Apenas Uma Vez!
+  ├
+  🧐─── *Stickers*
+  ├─ 🐊 ${prefix}sticker (Img > Sticker)
+  ├─ 🐊 ${prefix}stickergif (Img > Sticker)
+  ├─ 🐊 ${prefix}toimage (Sticker > Img)
+  ├>>> Marque a Imagem e escreva
+  ├
+  🧐─── *Interação*
+  ├─  ${prefix}meme
+  ├─  ${prefix}memeindo
+  ├─  ${prefix}simi (off)
+  ├─  ${prefix}ttp (texto em audio)
+  ├─  ${prefix}images
+  ├
+  🧐─── *Textos Fotos*
+  ├─  ${prefix}3dtext
+  ├─  ${prefix}blackbird
+  ├─  ${prefix}water
+  ├─  ${prefix}tts (texto para foto)
+  🧐─── _*Adminstração*_
+  ├─  ${prefix}abrirgrp (abrir grp)
+  ├─  ${prefix}fechargrp (fechar grp)
+  ├─  ${prefix}promover (promover a adm)
+  ├─  ${prefix}despromover (despromover a adm)
+  ├─  ${prefix}mencionar (chamar atenção de todos)
+  ├─  ${prefix}adicionar (adicionar membros)
+  ├─  ${prefix}banir (banir)
+  ├─  ${prefix}deletar (deleta mensagem do bot)
+  ├─  ${prefix}listadmins (lista de admins)
+  ├─  ${prefix}linkgroup (link desse grupo)
+  ├
+  🧐─── *Ativamentos*
+  ├─ 🐉 ${prefix}bemvindo (bem vindo)
+  ├─ 🐉 ${prefix}nsfw (porno)
+  ├─ 🐉 ${prefix}levelon (level ativar)
+  ├>>>> [1 para ativar]
+  ├
+  🧐─── *Stalkear pessoas*
+  ├─ ${prefix}tiktokstalk
+  ├  ${prefix}igstalk
+  ├ 
+  🧐─── *DOWNLOAD YOUTUBE*
+  │
+  ├─  ${prefix}play 
+  ├─  ${prefix}tomp3
+  ├─  ${prefix}ytsearch
+  ├─  ${prefix}ytmp3
+  ├─  ${prefix}ytmp2
+  ├─  ${prefix}ytmp1
+  ├─  ${prefix}mp3
+  ├─  ${prefix}ytmp4
+  ├  ${prefix}joox
+  ├
+  🧐───*OUTROS*
+  │
+  ├─  ${prefix}letras
+  ├─  ${prefix}wikien (wikipedia pesquisas)
+  ├─  ${prefix}nulis
+  ├─  ${prefix}quotes
+  ├─  ${prefix}quotemaker
+  ├─  ${prefix}trust
+  ├─  ${prefix}seulink (link whatsapp)
+  ├─  ${prefix}dare
+  ├─  ${prefix}destrava (destrava o zip zop)
+  └─  ${prefix}chord
+`
+}
+
+exports.help = help
